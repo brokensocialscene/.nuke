76897f48627fdcfa662c3cd79c65eecef22982ce
brokensocialscene <61168009+brokensocialscene@users.noreply.github.com>
2020-02-17 18:16:06 -0500

    Update init.py
    Initial init.py file from Gumroad Python for Nuke tutorial

--- init.py
+++ init.py
@@ -1,1 +1,11 @@
+# ---------------------------------------
+# init.py
+# Version 1.0.0
+# Last Updated: Feb 20, 2020
+# ---------------------------------------
+
+# ---- DEFINE CUSTOM FOLDER STRUCTURE----
+
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./pyton')
 
