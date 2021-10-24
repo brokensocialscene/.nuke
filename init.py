0d03b6037dd3639ad8e0cc2067afa43b08a281be
brokensocialscene <61168009+brokensocialscene@users.noreply.github.com>
2021-10-24 14:49:01 -0400

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,3 @@
-# ---------------------------------------
-# init.py
-# Version 1.0.0
-# Last Updated: Feb 20, 2020
-# ---------------------------------------
 
-# ---- DEFINE CUSTOM FOLDER STRUCTURE----
-
-nuke.pluginAddPath('./gizmos')
 nuke.pluginAddPath('./pyton')
 
